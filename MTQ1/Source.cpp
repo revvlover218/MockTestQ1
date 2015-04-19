3370ad207ec4f11fd939578ed1f0551a1bda2ad3
revvlover218 <reshalluchman@yahoo.com>
2015-04-19 17:37:39 +0200

    q1p3 done

--- MTQ1/Source.cpp
+++ MTQ1/Source.cpp
@@ -15,10 +15,10 @@ int main(int argc, char *argv[]) {
 	// 1.2 create and switch to branch "hello", uncomment (and complete) the following line and make a commit
 	// cout << "Hello, my name is ___________." << endl;
 	// 1.3 switch to the "master" branch, uncomment the following four lines and make a commit
-	// string name;
-	// cout << "Please enter your name: ";
-	// cin >> name;
-	// cout << "Hello, " << name << endl;
+	 string name;
+	 cout << "Please enter your name: ";
+	 cin >> name;
+	 cout << "Hello, " << name << endl;
 	// 1.4 merge branch "hello" into "master" keeping ALL of the code added so far
 	// 1.5 make a merge commit
 }
